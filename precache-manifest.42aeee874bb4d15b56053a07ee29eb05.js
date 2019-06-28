533c92b75e909fb235299ad8d7fc7299ee3ada6a
stanley man <stanleyman@stanleys-MBP.hitronhub.home>
2019-06-28 10:12:04 -0700

    Updates

---
+++ precache-manifest.42aeee874bb4d15b56053a07ee29eb05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63195fa106614cae052edf0327beb5d6",
+    "url": "/miniJSONParser/index.html"
+  },
+  {
+    "revision": "5a242001fa20f2b79426",
+    "url": "/miniJSONParser/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "58deffd46194b8b354b2",
+    "url": "/miniJSONParser/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "5a242001fa20f2b79426",
+    "url": "/miniJSONParser/static/js/2.77e6fb04.chunk.js"
+  },
+  {
+    "revision": "58deffd46194b8b354b2",
+    "url": "/miniJSONParser/static/js/main.60ab4462.chunk.js"
+  },
+  {
+    "revision": "54596b614fd953223f64",
+    "url": "/miniJSONParser/static/js/runtime~main.b1fbca0c.js"
+  }
+]);
